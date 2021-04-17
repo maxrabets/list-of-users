62f1d95c4da7e60cc64c81acb3ca7f41b841b25e
maxrabets <rabetsmaxim@gmail.com>
2021-04-17 14:00:09 +0300

    PATH var added to env

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require('express');
 const sequelize = require("./sequelize");
 const app = express();
 const cookieParser = require('cookie-parser')
-app.use(cookieParser("secret"));
 const registrationRouter = require("./routes/registrationRouter.js");
 const loginRouter = require("./routes/loginRouter.js");
 const homeRouter = require("./routes/homeRouter.js");
+require("dotenv").config();
 
 app.use(express.static(__dirname + "/public"));
 
 app.use("/registration", registrationRouter);;
 app.use("/login", loginRouter);
 app.use("/", homeRouter);
+app.use(cookieParser("secret"));
 
 sequelize.sync({force: false}).then(result=>{
   console.log("OK");
-  app.listen(3000,()=>{
-  console.log('Server is up and running at the port 3000')
+  app.listen(process.env.PORT,()=>{
+  console.log('Server is up and running at the port ' + process.env.PORT)
   })
 }).catch(err=> console.log(err));
