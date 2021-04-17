6425913ec9d35f8832540a4fa8e904b526a9fd89
maxrabets <rabetsmaxim@gmail.com>
2021-04-17 15:06:50 +0300

    postgress added

--- sequelize.js
+++ sequelize.js
@@ -1,12 +1,16 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: process.env.DATABASE_URL,
-  define: {
-    timestamps: false
-  }
+// const sequelize = new Sequelize({
+  // dialect: "sqlite",
+  // storage: process.env.DATABASE_URL,
+  // define: {
+    // timestamps: false
+  // }
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, "maxrabets", "123456", {
+  dialect: "postgres"
 });
 
 module.exports = sequelize;
